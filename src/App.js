73f0a19326cbfdc161e93d24ba62e56291bf46b9
Mayibongwe Sibanda <mayibongwes@gmail.com>
2024-03-24 22:30:07 +0200

    start- navlinks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import {Route} from 'react-router-dom';
+import Home from './components/Home';
+import Product from './components/Product';
+import Header from './components/layout/Header';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <Header/>
+    <main>
+      <Route path="/home">
+        <Home />
+      </Route>
+
+      <Route path="/products">
+        <Product />
+      </Route>
+    </main>
+
     </div>
   );
 }
---
+++ src/components/layout/Header.js
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom"
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+        <nav>
+            <ul>
+                <li><NavLink activeClassName="active" to='/home'>Home</NavLink></li>
+                <li><NavLink activeClassName="active" to='/products'>Products</NavLink></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
